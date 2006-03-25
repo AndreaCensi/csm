95539e4bf85d7059bad066b134389ca168bc0bc7
Andrea Censi <andrea@cds.caltech.edu>
2006-03-25 19:23:25 +0000

    spostate

--- matlab/plotVectors.m
+++
@@ -1,6 +0,0 @@
-function plotVectors(ref, points, color)
-	t = ref(1:2);
-	size(repmat(t,1,size(points,2)));
-	points2 =  rot(ref(3)) * points + repmat(t,1,size(points,2));
-	plot(points2(1,:),points2(2,:),color);
-
