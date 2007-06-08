cf5c80a771dfdef026ee5aceffa496684672fd8d
Andrea Censi <andrea@cds.caltech.edu>
2007-06-08 09:36:20 +0000

    fixing JSON

--- rsm/fix_json.rb
+++ rsm/fix_json.rb
@@ -7,37 +7,47 @@ require 'json/pure'
 module JSON
   module Pure
 
-    class Parser 
-      # Creates a new JSON::Pure::Parser instance for the string _source_.
-      def initialize(source)
-        super
-        @create_id = JSON.create_id
-        reset # added
-      end
+	class Parser
+     def initialize(source, opts = {})
+       super
+       if !opts.key?(:max_nesting) # defaults to 19
+         @max_nesting = 19
+       elsif opts[:max_nesting]
+         @max_nesting = opts[:max_nesting]
+       else
+         @max_nesting = 0
+       end
+       @create_id = JSON.create_id
+       reset
+     end
 
-      # Parses the current JSON string _source_ and returns the complete data
-      # structure as a result.
-      def parse
-#        reset
-        obj = nil
-        until eos? || obj    # added obj
-          case
-          when scan(OBJECT_OPEN)
-            obj and raise ParserError, "source '#{peek(20)}' not in JSON!"
-            obj = parse_object
-          when scan(ARRAY_OPEN)
-            obj and raise ParserError, "source '#{peek(20)}' not in JSON!"
-            obj = parse_array
-          when skip(IGNORE)
-            ;
-          else
-            raise ParserError, "source '#{peek(20)}' not in JSON!"
-          end
-        end
-        obj or raise ParserError, "source did not contain any JSON!"
-        obj
-      end
+
+     # Parses the current JSON string _source_ and returns the complete data
+     # structure as a result.
+     def parse
+       obj = nil
+       until eos? || obj
+         case
+         when scan(OBJECT_OPEN)
+           obj and raise ParserError, "source '#{peek(20)}' not in JSON!"
+           @current_nesting = 1
+           obj = parse_object
+         when scan(ARRAY_OPEN)
+           obj and raise ParserError, "source '#{peek(20)}' not in JSON!"
+           @current_nesting = 1
+           obj = parse_array
+         when skip(IGNORE)
+           ;
+         else
+           raise ParserError, "source '#{peek(20)}' not in JSON!"
+         end
+       end
+       obj or raise ParserError, "source did not contain any JSON!"
+       obj
+     end
+
 	end
 
   end
 end
+
