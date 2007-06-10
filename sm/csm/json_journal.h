a57544bdb8fc9ebd8fa68604d5cd27e48c8407b7
Andrea Censi <andrea@cds.caltech.edu>
2007-06-10 15:04:42 +0000

    Added (void).

--- sm/csm/json_journal.h
+++ sm/csm/json_journal.h
@@ -7,14 +7,14 @@
 
 #define JJ jj_enabled()
 
-int jj_enabled();
+int jj_enabled(void);
 
 void jj_context_enter(const char*context_name);
-void jj_context_exit();
+void jj_context_exit(void);
 
 void jj_loop_enter(const char*loop_name);
-void jj_loop_iteration();
-void jj_loop_exit();
+void jj_loop_iteration(void);
+void jj_loop_exit(void);
 
 void jj_add_int(const char*name, int);
 void jj_add_double(const char*name, double);
@@ -23,6 +23,6 @@ void jj_add_int_array(const char*name, int*,int);
 void jj_add(const char*name, JO);
 
 void jj_set_stream(FILE*);
-FILE*  jj_get_stream();
+FILE*  jj_get_stream(void);
 
 #endif
