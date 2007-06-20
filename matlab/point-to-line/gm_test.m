660e8f32b10b60b5c2f3adc501571b101364d64d
Andrea Censi <andrea@cds.caltech.edu>
2007-06-20 12:52:06 +0000

    Experiments with calibration

--- matlab/point-to-line/gm_test.m
+++ matlab/point-to-line/gm_test.m
@@ -1,4 +1,3 @@
-
 theta = deg2rad(4);
 t = [0.3; -0.2];
 
@@ -8,8 +7,13 @@ alpha = [deg2rad(0);deg2rad(10);deg2rad(20);deg2rad(50);deg2rad(-20);];
 noise = 0
 for i=1:size(p,2)
 	corr{i}.p = p(:,i);
-	corr{i}.q = rot(theta)*p(:,i)+t + randn(2,1)*noise;
+	corr{i}.q = (rot(theta)*p(:,i)+t) + randn(2,1)*noise;
 	corr{i}.C = vers(alpha(i))*vers(alpha(i))';
 %	corr{i}.C = eye(2);
 end
+
+res = general_minimization(corr);
+
+res.x
+
 
